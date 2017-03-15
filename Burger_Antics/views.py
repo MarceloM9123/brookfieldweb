4924535212ce982234a43bc4914a0b8a45450754
Marcelo <marcelomata91@gmail.com>
2017-03-14 21:09:00 -0500

    added new theme to burger antics project

--- Burger_Antics/views.py
+++ Burger_Antics/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'burgerantics/index.html')
+
+
+def two(request):
+    return render (request, 'touche/two.html')
